2faae30853957a57ff48154281a6c70a4a1115a6
Blade Nelson <powderblock@gmail.com>
2015-04-21 14:23:01 -0400

    Add comments,  add image padding
    In styles.css:
    -Update image padding by adding 10px padding on all sides (top, bottom,
    left and right)
    In script.js:
    -update comments.

--- javascript/script.js
+++ javascript/script.js
@@ -5,13 +5,16 @@ function load(params) {
     $.getJSON("http://www.reddit.com/r/pics/.json?jsonp=?", params, function (data) {
         var children = data.data.children;
         $.each(children, function (i, item) {
+		//LowerCase() so if the user puts in a link LiKE ThIS then the system will be able to handle it.
 			if (item.data.url.toLowerCase().indexOf("http://i.imgur.com/") >= 0){
 				if (item.data.url.toLowerCase().indexOf(".gifv") < 0){
-					$("<img/>").attr("src", item.data.url).appendTo("#images");
+					$("<img>").attr("src", item.data.url).appendTo("#images");
 				}
 			}
         });
         if (children && children.length > 0) {
+			//Last id = the last element of the children ids.
+			//Children in this case 
             lastId = children[children.length - 1].data.id;
         } else {
             lastId = undefined;
@@ -19,9 +22,12 @@ function load(params) {
     });
 }
 
+//Function to check scrolling:
 $(window).scroll(function () {
+	//If the user gets 5px above the bottom of the page:
 	if ($(window).scrollTop() >= $(document).height() - $(window).height() - 5) {
 		if (lastId) {
+			//Load the next batch of posts:
 			load({
 				after: 't3_' + lastId
 			});
@@ -29,4 +35,6 @@ $(window).scroll(function () {
 	}
 });
 
+//Call the load function
+//This is the function that loads all the images into #images:
 load();
