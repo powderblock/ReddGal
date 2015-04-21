4d8dd926616390a9a746ea0ca118ba66e477262a
Blade Nelson <powderblock@gmail.com>
2015-04-21 13:19:35 -0400

    Make Scrolling to Bottom Load Next Image
    In index:
    - Remove scroll function.
    
    In script.js:
    -Add scroll function in order to load next image.
    -Add checking for i.imgur and for .gifv
    
    In styles.css:
    -Shrink max image width.

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ function load(params) {
     $.getJSON("http://www.reddit.com/r/pics/.json?jsonp=?", params, function (data) {
         var children = data.data.children;
         $.each(children, function (i, item) {
-            $("<img/>").attr("src", item.data.url).appendTo("#images");
+			if (item.data.url.toLowerCase().indexOf("http://i.imgur.com/") >= 0){
+				if (item.data.url.toLowerCase().indexOf(".gifv") < 0){
+					$("<img/>").attr("src", item.data.url).appendTo("#images");
+				}
+			}
         });
         if (children && children.length > 0) {
             lastId = children[children.length - 1].data.id;
@@ -15,12 +19,14 @@ function load(params) {
     });
 }
 
-load();
+$(window).scroll(function () {
+	if ($(window).scrollTop() >= $(document).height() - $(window).height() - 5) {
+		if (lastId) {
+			load({
+				after: 't3_' + lastId
+			});
+		}
+	}
+});
 
-$('.after').click(function () {
-    if (lastId) {
-        load({
-            after: 't3_' + lastId
-        });
-    }
-})
+load();
