540707bdc8990a784ab2588653b52ea322e28125
Blade Nelson <powderblock@gmail.com>
2015-04-25 18:48:22 -0400

    Improve formatting
    In index.html:
    -fix spacing
    
    in script.js:
    -fix spacing
    -add .trim
    -increase window height min to 600.

--- javascript/script.js
+++ javascript/script.js
@@ -5,7 +5,7 @@ var urls = [];
 function load(params) {
     params = params || {};
     //If this gets called and textbox.value is empty, declare a var and set it equal to textbox.value:
-	if(textbox.value == ""){
+	if (textbox.value.trim() === "") {
 		var subreddit = "all";
 	}else{
 		var subreddit = textbox.value;
@@ -16,7 +16,11 @@ function load(params) {
 			if($.inArray(item.data.url, urls) == -1){
 				urls.push(item.data.url);
 				//LowerCase() so if the user puts in a link LiKE ThIS then the system will be able to handle it.
-				if (item.data.url.toLowerCase().indexOf(".jpeg") >= 0 || item.data.url.toLowerCase().indexOf(".jpg") >= 0 || item.data.url.toLowerCase().indexOf(".png") >= 0 && item.data.url.toLowerCase().indexOf(".gifv") < 0){
+				var urlLower = item.data.url.toLowerCase();
+				if (urlLower.indexOf(".jpeg") >= 0 ||
+				urlLower.indexOf(".jpg") >= 0 ||
+				urlLower.indexOf(".png") >= 0 &&
+				urlLower.indexOf(".gifv") < 0){
 					$('#images').append('<div class="item"><a href='+item.data.url+' target="_blank"><img src='+item.data.url+'></img></a><span class="caption">'+item.data.title+'<br><a href ="http://reddit.com'+item.data.permalink+'"target="_blank">View comments on reddit</a><br>Score: '+item.data.score+'</span></div>');
 				}
 			}
@@ -32,7 +36,7 @@ function load(params) {
 //Function to check scrolling:
 $(window).scroll(function () {
 	//If the user gets 5px above the bottom of the page:
-	if ($(window).scrollTop() >= $(document).height() - $(window).height() - 500) {
+	if ($(window).scrollTop() >= $(document).height() - $(window).height() - 600) {
 		if (lastId) {
 			//Load the next batch of posts:
 			load({ after: 't3_' + lastId });
